1e73096eb20da16cd259200825e1c5845441cc27
sai-github <saiediitm@gmail.com>
2025-03-09 23:08:28 +0530

    style: drawer overlap with navbar

--- src/components/InfoDrawer.tsx
+++ src/components/InfoDrawer.tsx
@@ -16,7 +16,7 @@ function InfoDrawer({
     <Dialog
       open={isOpen}
       onClose={() => setIsOpen(false)}
-      className="relative z-10"
+      className="relative z-[60]"
     >
       <DialogBackdrop
         transition
@@ -25,8 +25,8 @@ function InfoDrawer({
 
       <div className="fixed inset-0 overflow-hidden">
         <div className="absolute inset-0 overflow-hidden">
-          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-            <DialogPanel className="pointer-events-auto w-screen max-w-md transform transition duration-500 ease-in-out translate-x-0 sm:duration-700">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full sm:pl-10">
+            <DialogPanel className="pointer-events-auto w-screen sm:max-w-md transform transition duration-500 ease-in-out translate-x-0 sm:duration-700">
               <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                 <div className="px-4 sm:px-6">
                   <div className="flex items-start justify-between">
