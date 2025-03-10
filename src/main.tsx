24492251dc2d3f1f5463e103802b30d6dd926a1b
sai-github <saiediitm@gmail.com>
2025-03-10 22:46:52 +0530

    chore: tracking using clarity

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
+import Clarity from '@microsoft/clarity';
+
+const projectId = "qm4ruypc21";
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
